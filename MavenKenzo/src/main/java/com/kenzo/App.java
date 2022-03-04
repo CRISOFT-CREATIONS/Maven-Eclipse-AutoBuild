fd7ea49c9c37f9cb0e0f339291ceaaa0f5fa7770
Raghav Bhatnagar <raghavbhatnagarixtis@gmail.com>
2022-03-04 17:47:53 +0530

    8th Commit

--- MavenKenzo/src/main/java/com/kenzo/App.java
+++ MavenKenzo/src/main/java/com/kenzo/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Hi Kenzo 7");
+        System.out.println("Hi Kenzo 8");
     }
 }
